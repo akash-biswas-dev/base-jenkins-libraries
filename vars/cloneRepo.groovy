33eca12da2afc6b948826274e3d20cf4b06cbf35
Akash Biswas <111556960+akash-biswas-dev@users.noreply.github.com>
2025-12-15 10:19:45 +0530

    Add useEnv function for clone code using env.

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -3,3 +3,11 @@ def call(String url, String branch){
   git url: "${url}", branch: "${branch}"
   echo 'Code cloning successfull.'
 }
+
+def useEnv(){
+  def url = env.GIT_URL
+  def fullBranch = env.GIT_BRANCH
+  def branch = fullBranch.tokenize('/').last()
+  git url:"${url}", branch:"${branch}"
+  echo "Code clone successfull."
+}
